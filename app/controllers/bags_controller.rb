afea0e721a631b204b1068b29568dfe6ff40ad85
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-05 21:37:09 +0200

    improved player view

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -1,7 +1,6 @@
 class BagsController < ApplicationController
   def index
-    @bags = Bag.where(player_id: params[:player_id]).first
-    raise
+    @bags = Bag.where(player_id: params[:player_id])
   end
 
   def show
