1efa65d19fd3cf3aad3ff52919f7b43f684318a1
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-06 13:41:18 +0200

    deactivated turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require_tree .
 //= require typed.js/js/typed.js
 //= require vivus/dist/vivus.min.js
