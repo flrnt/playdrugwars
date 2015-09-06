d3ff18faae03fbcb7782846e4a1739deef01ec5b
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-06 13:32:22 +0200

    devise functions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,15 +7,28 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
     @drugs = Drug.all
-    @bag = Bag.where(player_id: @player.id).first
+    # @bag = Bag.where(player_id: @player.id).first
   end
 
   def new
   end
 
   def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @player.update(params_player)
+    redirect_to player_path
   end
 
   def destroy
+  end
+
+  private
+
+  def params_player
+    params.require(:player).permit(:name)
   end
 end
